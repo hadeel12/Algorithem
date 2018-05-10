fb3f802525901fda5dd8e986ed86008efb7c9171
Hadeel <hadeelalomary66@gmail.com>
2018-05-10 12:57:49 +0300

    update harmlessRandomNote

--- Harmless random note algorithm/Harmless-random-note.js
+++ Harmless random note algorithm/Harmless-random-note.js
@@ -1,18 +1,26 @@
 function harmlessRandomNote(note,magazine) {
-    var noteWords=note.replace(/\s+/g,' ').trim().split(' ');
-    var magazineWords=magazine.replace(/\s+/g,' ').trim().split(' ');
-    var noteWordIncluded=[];
-    for(var i=0;i<noteWords.length;i++){
-        if(magazineWords.includes(noteWords[i])) {
-            delete magazineWords[magazineWords.indexOf(noteWords[i])];
-            noteWordIncluded.push(true);
-        }else{
-            noteWordIncluded.push(false);
+    var noteWords=note.split(' ');
+    var magazineWords=magazine.split(' ');
+    var magazineWordRepetition={};
+    magazineWords.forEach(function (word) {
+        if(!magazineWordRepetition[word]){
+            magazineWordRepetition[word]=0;
         }
-    }
-    return noteWordIncluded.includes(false)?false:true;
+        magazineWordRepetition[word]++;
+    });
+    var validWord=true;
+    noteWords.forEach(function (word) {
+        if(magazineWordRepetition[word]){
+            magazineWordRepetition[word]--;
+            if(magazineWordRepetition[word]<0){
+                validWord=false;
+            }
+        }else{
+            validWord=false;
+        }
+    });
+    return validWord;
 }
-var note="this is a secret note for you from a secret admirer";
-var magazine="puerto rico is a place of great wonder and  excitement it has many secret waterfall locatoins that i am an admirer of you must secret hike quite a distance to find the places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited";
-console.log(harmlessRandomNote(note,magazine));
+console.log(harmlessRandomNote('this is a secret note for you from a secret admirer', 'puerto rico is a place of great wonder and excitement it has many secret waterfall locatoins that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited'));
+
 
