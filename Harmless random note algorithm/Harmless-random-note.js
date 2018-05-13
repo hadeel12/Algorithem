e6ad14ab57fc5e026a2823979f43afcfb04d2496
Hadeel <hadeelalomary66@gmail.com>
2018-05-13 13:30:50 +0300

    enhance harmlessRandomNote

--- Harmless random note algorithm/Harmless-random-note.js
+++ Harmless random note algorithm/Harmless-random-note.js
@@ -8,19 +8,17 @@ function harmlessRandomNote(note,magazine) {
         }
         magazineWordRepetition[word]++;
     });
-    var validWord=true;
-    noteWords.forEach(function (word) {
-        if(magazineWordRepetition[word]){
-            magazineWordRepetition[word]--;
-            if(magazineWordRepetition[word]<0){
-                validWord=false;
+    for(var i=0;i<noteWords.length;i++){
+        if(magazineWordRepetition[noteWords[i]]){
+            magazineWordRepetition[noteWords[i]]--;
+            if(magazineWordRepetition[noteWords[i]]<0){
+                return false;
             }
         }else{
-            validWord=false;
+            return false;
         }
-    });
-    return validWord;
+    }
+    return true;
 }
 console.log(harmlessRandomNote('this is a secret note for you from a secret admirer', 'puerto rico is a place of great wonder and excitement it has many secret waterfall locatoins that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited'));
-
 
