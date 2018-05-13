2611752fd7d39bff758b4784713143c7312abe48
Hadeel <hadeelalomary66@gmail.com>
2018-05-13 13:32:24 +0300

    enhance binary search

--- Binary search algorithm/Binary-search.js
+++ Binary search algorithm/Binary-search.js
@@ -1,16 +1,18 @@
-function BinarySearch(values, startIndex, endIndex, searchedValue) {
+function BinarySearch(values, searchedValue) {
+    return runBinarySearch(values,0, values.length-1, searchedValue);
+}
+
+function runBinarySearch(values, startIndex, endIndex, searchedValue) {
     var middleIndex = Math.floor((startIndex + endIndex) / 2);
     var middleValue = values[middleIndex];
 
     if (middleValue === searchedValue) return true;
-    else if (middleValue < searchedValue && endIndex-startIndex > 1) {
-        startIndex=middleIndex;
-        return BinarySearch(values, startIndex, endIndex, searchedValue);
-    } else if (middleValue > searchedValue && values.length > 1) {
-        endIndex=middleIndex;
-        return BinarySearch(values, startIndex, endIndex, searchedValue);
+    else if (middleValue < searchedValue && middleIndex < endIndex) {
+        return runBinarySearch(values, middleIndex  + 1 , endIndex, searchedValue);
+    } else if (middleValue > searchedValue && startIndex < middleIndex) {
+        return runBinarySearch(values, startIndex, middleIndex - 1, searchedValue);
     } else return false;
 }
 
 var values = [1, 2, 5, 12, 25,50,70,100];
-console.log(BinarySearch(values, 0, values.length-1, 100));
+alert(BinarySearch(values,100));
