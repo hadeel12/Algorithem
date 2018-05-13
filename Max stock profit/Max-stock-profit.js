4f8f68c0b71c57b644068806af8593c9aaae8523
Hadeel <hadeelalomary66@gmail.com>
2018-05-13 15:56:22 +0300

    enhance max stock profit

--- Max stock profit/Max-stock-profit.js
+++ Max stock profit/Max-stock-profit.js
@@ -1,20 +1,19 @@
-function maxStockProfit(stockPrices) {
+function maxStockProfit(stockPrices){
     var maxProfit = -1;
-    var buyPrice = 0;
-    var sellPrice = 0;
-    var changeBuyPrice = true;
-    for (var i = 0; i < stockPrices.length; i++) {
-        if (changeBuyPrice) buyPrice = stockPrices[i];
-        sellPrice = stockPrices[i + 1];
-        if (sellPrice < buyPrice) {
-            changeBuyPrice = true;
+    var buyPrice=stockPrices[0];
+    var sellPrice=stockPrices[1];
+    for(var i = 1; i < stockPrices.length; i++) {
+        if(buyPrice < sellPrice) {
+            var tempProfit = sellPrice - buyPrice;
+            if(tempProfit > maxProfit) maxProfit = tempProfit;
+            sellPrice = stockPrices[i+1];
         }
         else {
-            var Profit = sellPrice - buyPrice;
-            if (Profit > maxProfit) maxProfit = Profit;
-            changeBuyPrice = false;
+            buyPrice = stockPrices[i];
+            sellPrice = stockPrices[i+1];
         }
     }
     return maxProfit;
 }
+
 console.log(maxStockProfit([10,18,4,5,9,6,16,12]));
