17cbd49d61aae8f0fed39fa39b4e829273780339
Hadeel <hadeelalomary66@gmail.com>
2018-05-13 09:24:21 +0300

    Binary Search update

--- Binary search algorithm/Binary-search.js
+++ Binary search algorithm/Binary-search.js
@@ -1,5 +1,14 @@
-function BinarySearch(names,requiredName)
-{
-    return names.includes(requiredName)?true:false;
+function BinarySearch(numbers,requiredNumber) {
+    var middleIndex = Math.floor(numbers.length / 2);
+    var middleNumber = numbers[middleIndex];
+
+    if (middleNumber === requiredNumber) return true;
+    else if (middleNumber < requiredNumber && numbers.length > 1) {
+        return BinarySearch(numbers.splice(middleIndex, numbers.length), requiredNumber);
+    }
+    else if (middleNumber > requiredNumber && numbers.length > 1) {
+        return BinarySearch(numbers.splice(0, middleIndex), requiredNumber);
+    }
+    else return false;
 }
-console.log(BinarySearch(['hadeel','ali','omar','ola','amer','ameen','momen','mamon','hala','alia','raghad','anwar'],'alia'));
+console.log(BinarySearch([5, 7, 12, 16, 36, 39, 42, 56, 71], 56));
